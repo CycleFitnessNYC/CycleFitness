5c32fbe70d67e4058d2f724d6e0bd5d08b8f8267
dlschon <dlschon0@gmail.com>
2015-01-03 12:24:29 -0600

    Made some stuff work

--- js/index.js
+++ js/index.js
@@ -2,53 +2,60 @@
 // http://packery.metafizzy.co/packery.pkgd.js
 // http://draggabilly.desandro.com/draggabilly.pkgd.js
 
+var $slot = $(".slot");
+var $packery;
+var bikeInfo;
+
 $( function()
 {
   //Make Draggabilly work with scaling
   Draggabilly.prototype.positionDrag = Draggabilly.prototype.setLeftTop;
 
   //Parse bikeinfo JSON into bikeInfo object
-  var bikeInfo = null;
   $.getJSON("/CycleFitness/json/bikeinfo.json", function(data) {
     console.log(data);
-    initPackery(data);
+    bikeInfo = data;
+    initPackery();
   });
 
 });
 
-function initPackery(bikeInfo) {
-  //Get Packery container
-  var $contElem = $(".packery")[0];
+function initPackery()
+{
+  var index = 0;
+  //Create bike cards from JSON
+  bikeInfo.bikes.forEach(function(bike)
+  {
+    $(".packery").append('<div class="card" id="' + index.toString() + '"></div>');
+    index++;
+  });
 
-  //Init Packery
-  var $container = $(".packery").packery(
+  //Init packery object
+  $packery = $(".packery").packery(
       {
         columnWidth: 160,
-        rowHeight: 160,
-        isResizeBound: false
+        rowHeight: 160
       });
 
   //Init cards
   var cardIndex = 0;
-  $container.find('.card').each(function (i, itemElem) {
-    itemElem.style.content = "url(" + bikeInfo.bikes[0].image + ")";
-    // make element draggable with Draggabilly
-    var draggie = new Draggabilly(itemElem);
-    draggie.on("dragEnd", function (draggieInstance, event, pointer) {
-      if (!(testRects($contElem.getBoundingClientRect(), itemElem.getBoundingClientRect())))
-        $container.packery("remove", itemElem);
-      else {
-        itemElem.classList.add("post-post-drag");
-      }
-    });
-
-    itemElem.onmouseout = function () {
-      itemElem.classList.remove("post-post-drag");
-    };
-    // bind Draggabilly events to Packery
-    $container.packery('bindDraggabillyEvents', draggie);
+  $packery.find('.card').each(function (i, itemElem)
+  {
+    initCard(itemElem)
   });
 
+}
+
+function initCard(itemElem)
+{
+  setImage(itemElem, bikeInfo);
+
+  makeDraggabilly(itemElem);
+
+  itemElem.onmouseout = function ()
+  {
+    itemElem.classList.remove("post-post-drag");
+  };
 }
 
 // Test two ClientRects for overlap
@@ -63,4 +70,129 @@ function testRects(r1, r2)
   if (r1.bottom < r2.top)
     return false;
   return true;
+}
+
+//Set image for a given card div
+function setImage(elem, bikeInfo)
+{
+  elem.style.content = "url(" + bikeInfo.bikes[parseInt(elem.id)].image + ")";
+}
+
+//Make an element into a Draggability object
+function makeDraggabilly(itemElem)
+{
+  // make element draggable with Draggabilly
+  var draggie = new Draggabilly(itemElem);
+
+  var dragEndUnslotted = function(draggie, event, pointer)
+  {
+    itemElem.classList.add("post-post-drag");
+
+    //Get slot with largest overlap area
+    var slotEntered;
+    var maxOverlap = 0;
+    $slot.each(function(i, slotElem)
+    {
+      var itemRect = itemElem.getBoundingClientRect();
+      var slotRect = slotElem.getBoundingClientRect();
+      //Calculate the area overlapped
+      var xOverlap = Math.max(0, Math.min(itemRect.right, slotRect.right) - Math.max(itemRect.left, slotRect.left));
+      var yOverlap = Math.max(0, Math.min(itemRect.bottom, slotRect.bottom) - Math.max(itemRect.top, slotRect.top));
+      var overlap = xOverlap * yOverlap;
+      if (overlap > maxOverlap && slotElem.childNodes.length == 0)
+      {
+        maxOverlap = overlap;
+        slotEntered = slotElem;
+      }
+    });
+
+    if (slotEntered)
+    {
+        if (slotEntered.children.length == 0)  //Is the slot empty?
+        {
+          //Remove card from the packery
+          $packery.packery("remove", itemElem);
+
+          //Add identical card to the slot
+          $(slotEntered).append('<div class="card slotted" id="' + itemElem.id + '"></div>');
+
+          var cardElem = slotEntered.childNodes[0];
+          initCard(cardElem);
+        }
+    }
+  }
+
+  var dragEndSlotted = function(draggie, event, pointer)
+  {
+    itemElem.classList.add("post-post-drag");
+
+    //Check for each packery (although there should only be 1)
+    $(".packery").each(function(i, packeryElem)
+    {
+      if (testRects(packeryElem.getBoundingClientRect(), itemElem.getBoundingClientRect()))
+      {
+        //Remove card from the slot
+        itemElem.remove();
+
+        //Add identical card to the packery
+        var cardElem = $.parseHTML('<div class="card" id="' + itemElem.id + '"></div>')[0];
+        $packery.append(cardElem);
+        $packery.packery("appended", cardElem);
+        initCard(cardElem);
+        return;
+      }
+    });
+
+    //Get slot with largest overlap area
+    var slotEntered;
+    var maxOverlap = 0;
+    $slot.each(function(i, slotElem)
+    {
+      var itemRect = itemElem.getBoundingClientRect();
+      var slotRect = slotElem.getBoundingClientRect();
+
+      //Calculate the area overlapped
+      var xOverlap = Math.max(0, Math.min(itemRect.right, slotRect.right) - Math.max(itemRect.left, slotRect.left));
+      var yOverlap = Math.max(0, Math.min(itemRect.bottom, slotRect.bottom) - Math.max(itemRect.top, slotRect.top));
+      var overlap = xOverlap * yOverlap;
+
+      if (overlap > maxOverlap && slotElem.childNodes.length == 0)
+      {
+        maxOverlap = overlap;
+        slotEntered = slotElem;
+      }
+    });
+
+    if (slotEntered)
+    {
+      if (slotEntered.children.length == 0)  //Is the slot empty?
+      {
+        //Remove card from the slot
+        itemElem.remove();
+
+        //Add identical card to the slot
+        $(slotEntered).append('<div class="card slotted" id="' + itemElem.id + '"></div>');
+
+        var cardElem = slotEntered.childNodes[0];
+        initCard(cardElem);
+      }
+    }
+    else
+    {
+      draggie.setLeftTop(0, 0);
+    }
+  }
+
+  var isSlotted = itemElem.classList.contains("slotted");
+  if (isSlotted)
+  {
+    draggie.on("dragEnd", dragEndSlotted);
+  }
+  else
+  {
+    draggie.on("dragEnd", dragEndUnslotted);
+  }
+
+  // bind Draggabilly events to Packery
+  $packery.packery('bindDraggabillyEvents', draggie);
 }
